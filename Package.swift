7c548a0782bd6286abf1c0c692e805e340588afa
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-02-06 10:47:36 -0200

    Bumping Package version to 5.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
 // Use of this file is governed by the BSD 3-clause license that
 // can be found in the LICENSE.txt file in the project root.
@@ -20,5 +20,6 @@ let package = Package(
         .testTarget(
             name: "Antlr4Tests",
             dependencies: ["Antlr4"])
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
